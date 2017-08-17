4e6dde5492076423f0cbbb416f5218845bd449e2
bingo-bomb <changcheng_jiang@163.com>
2017-08-17 14:07:34 +0800

    添加daydayup 记录平时框架

--- bingo-bomb-chaos/bingo-bomb-chaos-restful-api/src/test/java/org/bingo/bomb/chaos/restful/api/user/TestUserRestfulApi.java
+++ bingo-bomb-chaos/bingo-bomb-chaos-restful-api/src/test/java/org/bingo/bomb/chaos/restful/api/user/TestUserRestfulApi.java
@@ -14,12 +14,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 public class TestUserRestfulApi extends ConfigMainTest {
 	@Test
 	public void testfindByUserId() throws Exception {
-		MvcResult vcResult = this.mockMvc
+		MvcResult mvcResult = this.mockMvc
 				.perform((MockMvcRequestBuilders.post("/userRest/findByUserId/27613346230685810337309166528"))
 				.accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
 				.andReturn();
-		if (vcResult.getResponse().getStatus() == 200) {
-			String contentAsString = vcResult.getResponse().getContentAsString();
+		if (mvcResult.getResponse().getStatus() == 200) {
+			String contentAsString = mvcResult.getResponse().getContentAsString();
 			logger.info("请求成功,返回结果:" + contentAsString);
 		}
 	}
