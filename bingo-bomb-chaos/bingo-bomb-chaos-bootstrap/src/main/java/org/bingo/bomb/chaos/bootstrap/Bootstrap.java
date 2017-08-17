5cc9cd19fcb64bac9137e040ae3c2e7208ef9c70
bingo-bomb <changcheng_jiang@163.com>
2017-08-17 13:03:44 +0800

    mark

--- bingo-bomb-chaos/bingo-bomb-chaos-bootstrap/src/main/java/org/bingo/bomb/chaos/bootstrap/Bootstrap.java
+++ bingo-bomb-chaos/bingo-bomb-chaos-bootstrap/src/main/java/org/bingo/bomb/chaos/bootstrap/Bootstrap.java
@@ -5,7 +5,7 @@ import com.alibaba.dubbo.container.Main;
 /**
  * 项目启动
  * @author jiangchangcheng  
- * @date 2017年8月15日 下午3:06:34
+ * @date 2017年8月16日 下午3:06:34
  * @since JDK 1.7
  */
 public class Bootstrap {
