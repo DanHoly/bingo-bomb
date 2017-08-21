47f70c9b4f0480908792c002839bf93f547a5a0c
luoyecanyang <2364518488@qq.com>
2017-08-21 23:51:49 +0800

    编码从废话开始；湛多勇

--- bingo-bomb-daydayup/bingo-bomb-daydayup-jvm/src/main/java/org/bingo/bomb/daydayup/jvm/App.java
+++ bingo-bomb-daydayup/bingo-bomb-daydayup-jvm/src/main/java/org/bingo/bomb/daydayup/jvm/App.java
@@ -6,6 +6,7 @@ package org.bingo.bomb.daydayup.jvm;
  */
 public class App {
 	public static void main(String[] args) {
+		System.out.println("万丈高楼平地起,深圳一步他青云");
 		System.out.println("Hello World!");
 	}
 }
